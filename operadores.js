287056387e0b5c92bc4201b26ea3ac300e12ea41
Adriana <adrimedinamontiel12@gmail.com>
2023-09-11 15:46:33 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// Operadores matematicos
+// + - * / % **
+// += -= *= /= %= **=
+
+console.log(10+10);
+console.log(10-10);
+console.log(10/10);
+console.log(10*10);
+console.log(10%10);
+console.log(10**10); //pontencia
+let num = 15;
+console.log(num +=5); //num = num+5
+console.log(num -=5);
+console.log(num *=5);
+console.log(num /=5);
+console.log(num %=5);
+console.log(num **=5);
+
+//operadores booleanos
+// < > <= >= !== === 
+console.log(1<2);
+console.log(1>2);
+console.log(1<=2);
+console.log(1>=2);
+console.log(1!==2);
+console.log(1===2);
+
+//operadores logicos
+// and &&
+console.log(true && false);
+// or ||
+console.log(true || false);
+// not !
+console.log(!true);
